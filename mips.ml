a061a50583335258d96e22ff8fd4c0e44046a234
beshoux <beshoux@outlook.fr>
2022-12-21 19:24:55 +0100

    Implem instructions mips.

--- mips.ml
+++ mips.ml
@@ -1,3 +1,12 @@
 type reg = V0 | A0 | SP | RA
 type label = string
 type addr = Lbl of label | Mem of reg * int
+
+type instr =
+  | Addi of reg * reg * int
+  | La of addr * addr
+  | Li of addr * int
+  | Lw of addr * addr
+  | Move of reg * reg
+  | Jr of addr
+  | Jal of label
