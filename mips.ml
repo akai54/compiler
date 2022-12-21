f2d2036541b2a8742a42f675316a67452c71c2d3
beshoux <beshoux@outlook.fr>
2022-12-21 21:20:54 +0100

    mips fini.

--- mips.ml
+++ mips.ml
@@ -15,3 +15,19 @@ type instr =
 
 type dctv = Asciiz of string
 type dcl = label * dctv
+
+let fmt_reg = function V0 -> "$v0" | A0 -> "$a0" | SP -> "$sp" | RA -> "$ra"
+let fmt_addr = function Lbl l -> l
+
+let fmt_instr = function
+  | Li (r, i) -> ps "  li %s, %d" (fmt_addr r) i
+  | La (r, a) -> ps "  la %s, %s" (fmt_addr r) (fmt_addr a)
+
+let fmt_dir = function Asciiz s -> ps ".asciiz \"%s\"" s
+
+let emit oc asm =
+  Printf.fprintf oc ".text\n.globl main\nmain:\n";
+  List.iter (fun i -> Printf.fprintf oc "%s\n" (fmt_instr i)) asm.text;
+  Printf.fprintf oc "  syscall\n  jr $ra\n";
+  Printf.fprintf oc "\n.data\n";
+  List.iter (fun (l, d) -> Printf.fprintf oc "%s: %s\n" l (fmt_dir d)) asm.data
