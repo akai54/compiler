a05c022de767c8177f386271fbb47b35e5277f16
beshoux <beshoux@outlook.fr>
2022-12-27 11:25:35 +0100

    Correction asm dans mips

--- mips.ml
+++ mips.ml
@@ -15,6 +15,7 @@ type instr =
 
 type dctv = Asciiz of string
 type dcl = label * dctv
+type asm = { text : instr list; data : dcl list }
 
 let fmt_reg = function V0 -> "$v0" | A0 -> "$a0" | SP -> "$sp" | RA -> "$ra"
 let fmt_addr = function Lbl l -> l
@@ -28,6 +29,6 @@ let fmt_dir = function Asciiz s -> ps ".asciiz \"%s\"" s
 let emit oc asm =
   Printf.fprintf oc ".text\n.globl main\nmain:\n";
   List.iter (fun i -> Printf.fprintf oc "%s\n" (fmt_instr i)) asm.text;
-  Printf.fprintf oc "  syscall\n  jr $ra\n";
+  Printf.fprintf oc "  move $a0, $v0\n  li $v0, 1\n  syscall\n  jr $ra\n";
   Printf.fprintf oc "\n.data\n";
   List.iter (fun (l, d) -> Printf.fprintf oc "%s: %s\n" l (fmt_dir d)) asm.data
