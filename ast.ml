6bb7938a5512c0489075baf87c0d24f9881e8ac7
beshoux <beshoux@outlook.fr>
2022-12-27 18:30:26 +0100

    Fix ast.

--- ast.ml
+++ ast.ml
@@ -4,17 +4,13 @@ module type Parameters = sig
   type value
 end
 
-module Values = struct
-  type value = Void | Int of int | Bool of bool | Str of string
-end
-
 module Syntax = struct
   type expr =
-    | Value of { value : Values.value; pos : Lexing.position }
-    | Call of { func : ident; args : expr list; pos : Lexing.position }
-    | Var of { name : ident; pos : Lexing.position }
+    | Bool of { value : bool; pos : Lexing.position }
+    | Int of { value : int; pos : Lexing.position }
+    | String of { value : string; pos : Lexing.position }
 end
 
 module IR (P : Parameters) = struct
-  type expr = Value of P.value | Call of ident * expr list | Var of ident
+  type expr = Value of P.value
 end
