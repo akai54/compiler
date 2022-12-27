e10a4d6dca7d8e188c68ade122010b7ab8eec2ad
beshoux <beshoux@outlook.fr>
2022-12-27 18:35:54 +0100

    Fix mips.

--- mips.ml
+++ mips.ml
@@ -23,7 +23,6 @@ let fmt_dir = function Asciiz s -> ps ".asciiz \"%s\"" s
 
 let emit oc asm =
   Printf.fprintf oc ".text\n.globl main\nmain:\n";
-  List.iter (fun i -> Printf.fprintf oc "%s\n" (fmt_instr i)) asm.text;
   Printf.fprintf oc "  move $a0, $v0\n  li $v0, 1\n  syscall\n  jr $ra\n";
   Printf.fprintf oc "\n.data\n";
   List.iter (fun (l, d) -> Printf.fprintf oc "%s: %s\n" l (fmt_dir d)) asm.data
